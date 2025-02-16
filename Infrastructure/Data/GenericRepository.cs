7f5d656b83aec707e28dab8d88d88a04e4cfda47
Izzat Jamili <gih.nih@gmail.com>
2025-02-16 17:20:29 +0800

    29 - creating generic repository

---
+++ Infrastructure/Data/GenericRepository.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Interfaces;
+
+namespace Infrastructure.Data
+{
+    public class GenericRepository<T>(StoreContext context) : IGenericRepository<T>
+        where T : BaseEntity
+    {
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Exists(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T?> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<T>> ListAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SaveAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
